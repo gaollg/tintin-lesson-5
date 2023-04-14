03aeb4bdeec156c061ffe7d4ba0d8fa4dce89ef8
Gaollg <gaollg@qq.com>
2023-04-14 15:26:42 +0800

    使用新的合约

--- crowdfundinglesson5homework/src/crowd-funding-lesson-5-homework.ts
+++ crowdfundinglesson5homework/src/crowd-funding-lesson-5-homework.ts
@@ -1,35 +1,31 @@
 import {
   CampaignBidLog as CampaignBidLogEvent,
   CampaignLog as CampaignLogEvent
-} from "../generated/CrowdFundingLesson5Homework/CrowdFundingLesson5Homework"
-import { CampaignBidLog, CampaignLog } from "../generated/schema"
+} from '../generated/CrowdFundingLesson5Homework/CrowdFundingLesson5Homework';
+import { CampaignBidLog, CampaignLog } from '../generated/schema';
 
 export function handleCampaignBidLog(event: CampaignBidLogEvent): void {
-  let entity = new CampaignBidLog(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.campaignId = event.params.campaignId
-  entity.sender = event.params.sender
-  entity.amount = event.params.amount
+  let entity = new CampaignBidLog(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.campaignId = event.params.campaignId;
+  entity.sender = event.params.sender;
+  entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleCampaignLog(event: CampaignLogEvent): void {
-  let entity = new CampaignLog(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.campaignId = event.params.campaignId
-  entity.receiver = event.params.receiver
-  entity.goal = event.params.goal
+  let entity = new CampaignLog(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.campaignId = event.params.campaignId;
+  entity.receiver = event.params.receiver;
+  entity.goal = event.params.goal;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
